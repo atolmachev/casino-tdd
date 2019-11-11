e4f1895eb38d4c2765ee61a4a07d9eb0f15a4728
Andrei@psdtraining.de <andrey.tolmachev@gmail.com>
2019-11-11 07:30:13 +0300

    First revision

---
+++ src/test/java/com/atolmachev/casino/CasinoTest.java
@@ -0,0 +1,12 @@
+package com.atolmachev.casino;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CasinoTest {
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
